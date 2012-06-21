375ee6ecd9a1acefc885f2cec8953a733c19095b
darkcooger <cooger@darkcooger.net>
2012-06-20 22:01:16 -0500

    asdf

--- Server.js
+++ Server.js
@@ -7,8 +7,8 @@ var User = require("./User");
 
 var incomingConnection = function(socket)
 {
-	console.log(socket.manager.handshaken[socket.id].query.token);
-	socket.disconnect();
+	User.FromToken(socket.manager.handshaken[socket.id].query.token, socket);
+	setTimeout(socket.disconnect(), 3000);
 	//socket.addListener("connect", function(resource, token)
 	//{
 	//	console.log("Connect with token: " + token);
